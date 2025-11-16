e8a616de3548b24c787a4279b6c84a156b25059d
AlcadolL <renne1222@gmail.com>
2025-11-16 20:08:24 +0900

    Update script.js
    DPTを追加

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 javascript:(function(){
-    let d='//*[@id="wrapper"]/div[3]/div[2]/div[2]/div[2]/div[3]/div[3]/div[2]';
-    let t='//*[@id="wrapper"]/div[3]/div[2]/div[2]/div[2]/div[3]/div[6]/div[2]';
-    let dN=document.evaluate(d,document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
-    let tN=document.evaluate(t,document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
-    let dT=dN?dN.innerText.trim():"";
-    let tT=tN?tN.innerText.trim():"";
-    if(dT != "" && tT != ""){
-        let dI = parseInt(dT.replace(/,/g, ""), 10);
-        let tP = tT.split(":");
-        let tS = parseInt(tP[0], 10) * 60 + parseInt(tP[1], 10);
-        let dps = dI / tS;
+    let damage='//*[@id="wrapper"]/div[3]/div[2]/div[2]/div[2]/div[3]/div[3]/div[2]';
+    let time='//*[@id="wrapper"]/div[3]/div[2]/div[2]/div[2]/div[3]/div[6]/div[2]';
+    let turn='//*[@id="wrapper"]/div[3]/div[2]/div[2]/div[2]/div[3]/div[5]/div[2]';
+
+    let damageNode=document.evaluate(damage,document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
+    let timeNode=document.evaluate(time,document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
+    let turnNode=document.evaluate(turn,document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
+
+    let damageText=damageNode?damageNode.innerText.trim():"";
+    let timeText=timeNode?titeNode.innerText.trim():"";
+    let turnText=turnNode?turnNode.innerText.trim():"";
+
+    if(damageText != "" && timeText != "" && turnText != ""){
+        let damageInt = parseInt(damageText.replace(/,/g, ""), 10);
+
+        let timeParts = timeText.split(":");
+        let timeSeconds = parseInt(timeParst[0], 10) * 60 + parseInt(timeParts[1], 10);
+
+        let turnInt = parseInt(turnText, 10);
+        
+        let dps = damageInt / timeSeconds;
         let dpm = Math.floor(dps * 60);
+        let dpt = damageInt / turnInt
         alert(
             "DPM：" + dpm.toLocaleString()
+            + "\nDPT：" + dpt.toLocaleString()
         );
     }else{
         alert("");
     }
 })();
-
