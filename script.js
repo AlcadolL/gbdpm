2e7f14ed90280bdfa538a4b307e802c9ba5f12c3
AlcadolL <renne1222@gmail.com>
2025-11-16 19:45:43 +0900

    Update script.js
    計算式の変更

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 javascript:(function(){
-    let dmg='//*[@id="wrapper"]/div[3]/div[2]/div[2]/div[2]/div[3]/div[3]/div[2]';
-    let time='//*[@id="wrapper"]/div[3]/div[2]/div[2]/div[2]/div[3]/div[6]/div[2]';
-    let dmgNode=document.evaluate(dmg,document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
-    let timeNode=document.evaluate(time,document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
-    let dmgText=dmgNode?dmgNode.innerText.trim():"";
-    let timeText=timeNode?timeNode.innerText.trim():"";
-    if(dmgText != "" && timeText != ""){
-        let dmgInt = parseInt(dmgText.replace(/,/g, ""), 10);
-        let parts = timeText.split(":");
-        let timeSeconds = parseInt(parts[0], 10) * 60 + parseInt(parts[1], 10);
-        let dps = dmgInt / timeSeconds;
-        let dpm = Math.floor(dps / 60);
+    let d='//*[@id="wrapper"]/div[3]/div[2]/div[2]/div[2]/div[3]/div[3]/div[2]';
+    let t='//*[@id="wrapper"]/div[3]/div[2]/div[2]/div[2]/div[3]/div[6]/div[2]';
+    let dN=document.evaluate(d,document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
+    let tN=document.evaluate(t,document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue;
+    let dT=dN?dN.innerText.trim():"";
+    let tT=tN?tN.innerText.trim():"";
+    if(dT != "" && tT != ""){
+        let dI = parseInt(dT.replace(/,/g, ""), 10);
+        let tP = tT.split(":");
+        let tS = parseInt(tP[0], 10) * 60 + parseInt(tP[1], 10);
+        let dps = dI / tS;
+        let dpm = Math.floor(dps * 60);
         alert(
             "DPM：" + dpm.toLocaleString()
         );
@@ -18,3 +18,4 @@ javascript:(function(){
         alert("");
     }
 })();
+
